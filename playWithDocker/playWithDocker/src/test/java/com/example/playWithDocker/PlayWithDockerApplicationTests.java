fadeb0ef19d8cf8be4be00631ba3e9d66e3bb6ec
Rahul Biswas <45085998+rahulbiswaser@users.noreply.github.com>
2021-06-19 12:03:00 +0530

    Add files via upload

---
+++ playWithDocker/playWithDocker/src/test/java/com/example/playWithDocker/PlayWithDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.playWithDocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayWithDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
